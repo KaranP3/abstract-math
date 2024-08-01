9d5915514c71bc8d841e6ad62ac446a1b0107e80
KaranP3 <karanpillai93@gmail.com>
2024-08-01 06:43:37 +0530

    Add semigroups revision

--- src/main/scala/part2abstractmath/Semigroups.scala
+++ src/main/scala/part2abstractmath/Semigroups.scala
@@ -3,63 +3,58 @@ package part2abstractmath
 
 object Semigroups {
 
-  // semigroups COMBINE elements of the same type
+  // semigroups combine elements of the same type
   import cats.Semigroup
   import cats.instances.int._
+
+  val naturalIntSemigroup = Semigroup[Int]
+  val intCombination =  naturalIntSemigroup.combine(2, 46) // addition
+
   import cats.instances.string._
-  import cats.instances.option._
 
-  val naturalIntSemigroup: Semigroup[Int] = Semigroup[Int]
-  val intCombination: Int = naturalIntSemigroup.combine(1, 2) // addition
+  val naturalStringSemigroup = Semigroup[String]
+  val stringCombination = naturalStringSemigroup.combine("I love ", "Cats")
 
-  val naturalStringSemigroup: Semigroup[String] = Semigroup[String]
-  val stringCombination: String = naturalStringSemigroup.combine("Hello, ", "world") // concatenation
+  // specific API
+  def reduceInts(list: List[Int]): Int = list.reduce(naturalIntSemigroup.combine)
+  def reduceStrings(list: List[String]): String = list.reduce(naturalStringSemigroup.combine)
 
-  private def reduceInts(list: List[Int]): Int = list.reduce(naturalIntSemigroup.combine)
-  private def reduceStrings(list: List[String]): String = list.reduce(naturalStringSemigroup.combine)
-
-  // general reduction API - the power of semigroups
-  private def reduceThings[T](list: List[T])(implicit semigroup: Semigroup[T]): T =
-    list.reduce(semigroup.combine)
+  // general API
+  def reduceThings[T](list: List[T])(implicit semigroup: Semigroup[T]): T = list.reduce(semigroup.combine)
 
 
-  // TODO 1: support a new type
   case class Expense(id: Long, amount: Double)
 
-  implicit val ExpenseSemigroup: Semigroup[Expense] = Semigroup.instance[Expense] {(e1, e2) =>
+  implicit val expenseSemigroup: Semigroup[Expense] = Semigroup.instance[Expense] { (e1, e2) =>
     Expense(Math.max(e1.id, e2.id), e1.amount + e2.amount)
   }
 
-  // extension methods from semigroup - |+|
+  // extension methods from semigroups
   import cats.syntax.semigroup._
 
-  val anIntSum = 2 |+| 3
-  val aStringConcat = "we like " |+| "semigroups"
-  val aCombinedExpense = Expense(4, 80) |+| Expense(8, 56)
-
-  // TODO 2: implement reduceThings2
-  private def reduceThings2[T : Semigroup](list: List[T]): T = list.reduce(_ |+| _)
-
   def main(args: Array[String]): Unit = {
-    println(intCombination) // 3
-    println(stringCombination) // "Hello, world"
+    println(intCombination)
+    println(stringCombination)
 
+    // specific API
     val numbers = (1 to 10).toList
     println(reduceInts(numbers))
 
-    val strings = List("I'm ", "starting ", "to ", "enjoy ", "semigroups")
+    val strings = List("I like", "Scala")
     println(reduceStrings(strings))
 
     // general API
-    reduceThings(numbers)
-    reduceThings(strings)
+    println(reduceThings(numbers))
+    println(reduceThings(strings))
 
-    val numberOptions: List[Option[Int]] = numbers.map(n => Option(n))
-    println(reduceThings(numberOptions)) // an Option[Int] containing the sum of all numbers
+    import cats.instances.option._
+    val numberOptions = numbers.map(n => Option(n))
+    println(reduceThings(numberOptions))
 
-    // test todo 1
-    val expenses = List(Expense(1, 99), Expense(2, 33), Expense(3, 50))
+    val stringOptions = strings.map(s => Option(s))
+    println(reduceThings(stringOptions))
+
+    val expenses = List(Expense(1, 99), Expense(2, 35), Expense(43, 10))
     println(reduceThings(expenses))
-    println(reduceThings2(expenses))
   }
 }
